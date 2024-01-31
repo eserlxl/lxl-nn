6e61536f96adb5cfa474a027a6fe6a919746b01b
Eser KUBALI <eserkubali@yahoo.com.tr>
2024-01-31 14:22:39 +0300

    Format change

--- include/test/testResult.h
+++ include/test/testResult.h
@@ -7,30 +7,27 @@ typedef struct TestResult {
     std::vector<float> errorPercentage;
     double elapsedTime;
 
-    TestResult(uzi domainSize)
-    {
+    TestResult(uzi domainSize) {
         this->domainSize = domainSize;
     }
 
-    void calcError()
-    {
+    void calcError() {
         errorPercentage.clear();
-        for(auto x:correct){
-            errorPercentage.push_back(100.f * (1.f - std::max(0.f,x) / domainSize));
+        for (auto x:correct) {
+            errorPercentage.push_back(100.f * (1.f - std::max(0.f, x) / domainSize));
         }
     }
 
-    std::string print()
-    {
+    std::string print() {
         std::string text = "{ ";
-        for(uzi i=0;i<correct.size();i++)
-        {
-            std::string errorPercentageText = roundStr(errorPercentage[i],2);//std::to_string(errorPercentage[i]);
+        for (uzi i = 0; i < correct.size(); i++) {
+            std::string errorPercentageText = roundStr(errorPercentage[i], 2);//std::to_string(errorPercentage[i]);
             //errorPercentageText=explode(".",errorPercentageText)[0]+"."+explode(".",errorPercentageText)[1].substr(0,2);
-            text+="L"+std::to_string(correct.size()-i)+" => [ ✓: "+std::to_string((int)(correct[i]))+"/"+std::to_string(domainSize)+", !: "+errorPercentageText+"% ]";
-            if(i<correct.size()-1)text+=", ";
+            text += "L" + std::to_string(correct.size() - i) + " => [ ✓: " + std::to_string((int) (correct[i])) + "/" +
+                    std::to_string(domainSize) + ", !: " + errorPercentageText + "% ]";
+            if (i < correct.size() - 1)text += ", ";
         }
-        text+=" }";
+        text += " }";
 
         return text;
     }
